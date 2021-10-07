8b92e06e2a15f0b46f9932248ccaa539b2468c39
Wisienek <frascowoolf@gmail.com>
2021-10-07 22:10:08 +0200

    commands test - instanceof + workflow

--- tests/src/commands.test.ts
+++ tests/src/commands.test.ts
@@ -1,3 +1,4 @@
+import { SlashCommandBuilder } from '@discordjs/builders/dist/interactions/slashCommands/SlashCommandBuilder';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -6,17 +7,19 @@ describe("Testing commands", () => {
     const commands = fs.readdirSync( path.resolve("cmds/") )
         .filter( file => file.endsWith('.js') || file.endsWith('.ts') );
 
-    describe("Checking individual commands", ()=> {
+    describe("Checking individual commands", () => {
         for( const file of commands ) {
             describe(`Checking command: ${file}`, () => {
                 const command = require(`../../cmds/${file}`);
 
-                it(`Should have data, permission, execute() `, ()=>{
+                it(`Should have: data, permission, execute() `, () => {
                    expect(command).toHaveProperty("data");
                    expect(command).toHaveProperty("permission");
                    expect(command).toHaveProperty("execute");
-                });
 
+                   expect( command.data ).toBeInstanceOf( SlashCommandBuilder );
+                   expect( command.execute ).toBeInstanceOf( Function );
+                });
             });
         }
     });
