851fa253097452463061b499c119397b83f54040
Wisienek <frascowoolf@gmail.com>
2021-10-07 20:46:18 +0200

    tests - config + command.test

---
+++ tests/src/commands.test.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+describe("Testing commands", () => {
+
+    const commands = fs.readdirSync( path.resolve("cmds/") )
+        .filter( file => file.endsWith('.js') || file.endsWith('.ts') );
+
+    describe("Checking individual commands", ()=> {
+        for( const file of commands ) {
+            describe(`Checking command: ${file}`, () => {
+                const command = require(`../../cmds/${file}`);
+
+                it(`Should have data, permission, execute() `, ()=>{
+                   expect(command).toHaveProperty("data");
+                   expect(command).toHaveProperty("permission");
+                   expect(command).toHaveProperty("execute");
+                });
+
+            });
+        }
+    });
+});
+
+
